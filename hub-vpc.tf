7092128184c51963fcf4aa87b1b95383b216d8c8
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2025-06-23 14:12:38 +0100

    Subnet creation added to hub-vpc using dynamic list creation.

--- hub-vpc.tf
+++ hub-vpc.tf
@@ -12,3 +12,18 @@
         deployed-by = var.deployed_by
       }
     }
+
+    resource "aws_subnet" "hub-subnets" {
+      count = length(var.hub_subnet_cidrs)
+      vpc_id = aws_vpc.hub-vpc.id
+      cidr_block = element(var.hub_subnet_cidrs, count.index)
+
+      tags = {
+        Name = "Hub-Subnet ${count.index + 1}",
+        environment = var.environment,
+        project-code = var.project_code,
+        owner = var.owner
+        managed-by = var.config_tool,
+        deployed-by = var.deployed_by
+      }
+    }

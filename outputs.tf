f9cc121f555173048c7568e240f0a77d53b31d99
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2025-06-23 14:22:24 +0100

    Output for Hub Subnet Id's as a list added.

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@
       value = aws_vpc.hub-vpc.id
       description = "Value of the Hub VPC ID"
     }
+
+    output "hub_subnet_ids" {
+        value = aws_subnet.hub-subnets[*].id
+        description = "All Hub Subnet ID's"
+    }

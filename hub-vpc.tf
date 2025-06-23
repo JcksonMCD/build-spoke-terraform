9963d1e5898644f37b4965168bac8f5c2f763728
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2025-06-23 13:45:35 +0100

    Hub VPC terraform file created and resource added for Hub VPC.

---
+++ hub-vpc.tf
@@ -0,0 +1,14 @@
+    resource "aws_vpc" "hub-vpc" {
+      cidr_block = var.vpc_cidr
+      enable_dns_hostnames = false
+      enable_dns_support = true
+
+      tags = {
+        Name = "VPC",
+        environment = var.environment,
+        project-code = var.project_code,
+        owner = var.owner
+        managed-by = var.config_tool,
+        deployed-by = var.deployed_by
+      }
+    }
